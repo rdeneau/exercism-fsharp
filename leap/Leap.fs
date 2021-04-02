181072408689f5a455b805d61d4ddd401b5bdfab
Romain Deneau <rdeneau@yahoo.fr>
2021-04-02 18:33:26 +0200

    Leap

---
+++ leap/Leap.fs
@@ -0,0 +1,13 @@
+﻿module Leap
+
+let (|DivisibleBy|_|) factor year =
+  match year % factor with
+  | 0 -> Some ()
+  | _ -> None
+
+let leapYear year =
+  match year with
+  | DivisibleBy 400 _ -> true
+  | DivisibleBy 100 _ -> false
+  | DivisibleBy   4 _ -> true
+  | _                 -> false
