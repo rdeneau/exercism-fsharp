79b102e9ce6249871a82d72c5519fb9bc92811df
Romain Deneau <rdeneau@yahoo.fr>
2021-04-02 18:33:59 +0200

    Accumulate

---
+++ accumulate/Accumulate.fs
@@ -0,0 +1,20 @@
+﻿module Accumulate
+
+/// V1: using List.rev
+let accumulateV1 (func: 'a -> 'b) (input: 'a list): 'b list =
+    let rec loop rest result =
+        match rest with
+        | [] -> result
+        | head :: tail -> loop tail (func head :: result)
+
+    loop input []
+    |> List.rev
+
+/// V2: full implementation
+let accumulate (func: 'a -> 'b) (input: 'a list): 'b list =
+    let rec loop rest result =
+        match rest with
+        | [] -> result
+        | head :: tail -> loop tail (result @ [ func head ])
+
+    loop input []
