7e08a0b149702d81ae6fe4a91181ef6c2f052251
Romain Deneau <rdeneau@yahoo.fr>
2021-04-02 18:34:19 +0200

    Grade School

---
+++ grade-school/GradeSchool.fs
@@ -0,0 +1,22 @@
+﻿module GradeSchool
+
+type School = Map<int, string list>
+
+let empty: School = Map.empty
+
+let add (student: string) (grade: int) (school: School): School =
+    school |> Map.change grade
+        (   function
+            | Some xs -> Some (student :: xs |> List.sort)
+            | None    -> Some [student] )
+
+let roster (school: School): string list =
+    school
+    |> Map.toList
+    |> List.sortBy fst
+    |> List.collect snd
+
+let grade (number: int) (school: School): string list =
+    school
+    |> Map.tryFind number
+    |> Option.defaultValue []
