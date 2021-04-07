31d1dc3919ce56585526a4dfe9abbdcd2c2e5c4a
Romain Deneau <rdeneau@yahoo.fr>
2021-04-07 19:13:17 +0200

    Armstrong numbers

---
+++ armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,14 @@
+﻿module ArmstrongNumbers
+
+let powerOf n x = pown x n
+
+let isArmstrongNumber (number: int): bool =
+    let digits = number |> string
+
+    let result =
+        digits
+        |> Seq.map (string >> int)
+        |> Seq.map (powerOf digits.Length)
+        |> Seq.sum
+
+    number = result
