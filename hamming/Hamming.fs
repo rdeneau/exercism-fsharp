d20a8ec75505a76c4773bd904df4d37968eabf1e
Romain Deneau <rdeneau@yahoo.fr>
2021-04-09 19:20:32 +0200

    Hamming solution 2: using Seq fold2

--- hamming/Hamming.fs
+++ hamming/Hamming.fs
@@ -1,11 +1,15 @@
 ﻿module Hamming
 
+let private incrementDifferences differences a b =
+    if a = b then
+        differences
+    else
+        differences + 1
+
 let distance (strand1: string) (strand2: string): int option =
     if strand1.Length = strand2.Length then
         (strand1, strand2)
-        ||> Seq.zip
-        |> Seq.filter (fun (a, b) -> a <> b)
-        |> Seq.length
+        ||> Seq.fold2 incrementDifferences 0
         |> Some
     else
         None
