301b9f07e6554a8de17e6ff267c8581cd795fe68
Romain Deneau <rdeneau@yahoo.fr>
2021-04-09 19:19:49 +0200

    Hamming solution 1: using Seq zip filter length

---
+++ hamming/Hamming.fs
@@ -0,0 +1,11 @@
+﻿module Hamming
+
+let distance (strand1: string) (strand2: string): int option =
+    if strand1.Length = strand2.Length then
+        (strand1, strand2)
+        ||> Seq.zip
+        |> Seq.filter (fun (a, b) -> a <> b)
+        |> Seq.length
+        |> Some
+    else
+        None
