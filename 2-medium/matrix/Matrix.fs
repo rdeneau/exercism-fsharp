c02243a1d3ed73f8b21af8d0cf7daa4b384e6b28
Romain Deneau <rdeneau@yahoo.fr>
2021-04-12 18:26:45 +0200

    Matrix (Medium): row function

---
+++ 2-medium/matrix/Matrix.fs
@@ -0,0 +1,10 @@
+﻿module Matrix
+
+let row num (matrix: string) =
+    let rows =
+        matrix.Split('\n')
+        |> List.ofArray
+        |> List.map (fun s -> s.Split(' ') |> List.ofArray |> List.map int)
+    rows.[num-1]
+
+let column index matrix = failwith "You need to implement this function."
