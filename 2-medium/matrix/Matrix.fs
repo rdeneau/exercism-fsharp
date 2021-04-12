32efe3e1c3887a1dff6fbd03e3ff9e71ce0f2d97
Romain Deneau <rdeneau@yahoo.fr>
2021-04-12 18:39:26 +0200

    Matrix: column function + refacto using itemAt helper

--- 2-medium/matrix/Matrix.fs
+++ 2-medium/matrix/Matrix.fs
@@ -1,10 +1,16 @@
 ﻿module Matrix
 
-let row num (matrix: string) =
-    let rows =
-        matrix.Split('\n')
-        |> List.ofArray
-        |> List.map (fun s -> s.Split(' ') |> List.ofArray |> List.map int)
-    rows.[num-1]
+let private rowsOf (matrix: string) =
+    matrix.Split('\n')
+    |> List.ofArray
+    |> List.map (fun s -> s.Split(' ') |> List.ofArray |> List.map int)
 
-let column index matrix = failwith "You need to implement this function."
+let private itemAt num = List.item (num - 1)
+
+let row num (matrix: string) =
+    rowsOf matrix
+    |> itemAt num
+
+let column num (matrix: string) =
+    rowsOf matrix
+    |> List.map (itemAt num)
