7449b5f8563a5ccec5572a83bb9cc993f9a3de2d
Romain Deneau <rdeneau@yahoo.fr>
2021-04-12 15:20:53 +0200

    Anagram (Medium)

---
+++ 2-medium/anagram/Anagram.fs
@@ -0,0 +1,15 @@
+﻿module Anagram
+
+open System
+
+let private normalize (str: string) =
+    str.ToUpperInvariant()
+    |> Seq.sort
+    |> String.Concat
+
+let private isAnagramOf target source =
+    normalize target = normalize source
+    && not (String.Equals(source, target, StringComparison.InvariantCultureIgnoreCase))
+
+let findAnagrams sources target =
+    sources |> List.filter (isAnagramOf target)
