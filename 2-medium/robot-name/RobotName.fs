3ed6e4bf1535c022a2af535ff75920e2bf654127
Romain Deneau <rdeneau@yahoo.fr>
2021-04-12 11:35:16 +0200

    Robot name

---
+++ 2-medium/robot-name/RobotName.fs
@@ -0,0 +1,38 @@
+﻿module RobotName
+
+open System
+
+type Robot = Robot of string
+
+let name (Robot x) = x
+
+let private random = Random(Guid.NewGuid().GetHashCode());
+
+let private randomRobotName () =
+    let letter () = ['A'..'Z'].[random.Next(0, 25)]
+    let digit () = random.Next(0, 9)
+    $"{letter()}{letter()}{digit()}{digit()}{digit()}"
+
+let mutable names: string Set = Set.empty
+
+let (|Available|Taken|) name =
+    if names |> Set.contains name
+    then Taken
+    else Available name
+
+let rec private uniqueRobotName () =
+    match randomRobotName () with
+    | Available name -> name
+    | Taken -> uniqueRobotName()
+
+let private mkRobotWith adjustNames =
+    let name = uniqueRobotName()
+    names <-
+        names
+        |> adjustNames
+        |> Set.add name
+    Robot name
+
+let mkRobot () = mkRobotWith id
+
+let reset (Robot previousName) = mkRobotWith (Set.remove previousName)
