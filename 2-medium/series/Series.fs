a16e32760b959676a2bafb439ccbd661319469ad
Romain Deneau <rdeneau@yahoo.fr>
2021-04-12 14:45:09 +0200

    Series (without Seq.windowed)

---
+++ 2-medium/series/Series.fs
@@ -0,0 +1,9 @@
+﻿module Series
+
+let slices (str: string) length =
+    let nbSlices = 1 + str.Length - length
+    if nbSlices <= 0 || length <= 0 then
+        None
+    else
+        Some [ for n in 1 .. nbSlices do
+                str.Substring(n - 1, length) ]
