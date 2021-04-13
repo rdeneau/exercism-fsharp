a3499b4d187b7be720f0bf0bfe3b5c986a389c09
Romain Deneau <rdeneau@yahoo.fr>
2021-04-13 16:17:58 +0200

    Binary search

---
+++ 2-medium/binary-search/BinarySearch.fs
@@ -0,0 +1,20 @@
+﻿module BinarySearch
+
+type ComparisonResult = Lower | Equal | Higher
+
+let compare a b =
+    if a = b then Equal
+    elif a < b then Lower
+    else Higher
+
+let find (inputs: int []) (value: int) =
+    let rec loop (inputs: int []) offset =
+        match inputs.Length with
+        | 0 -> None
+        | _ ->
+            let middleIndex = inputs.Length / 2
+            match compare value inputs.[middleIndex] with
+            | Equal  -> Some (offset + middleIndex)
+            | Lower  -> loop inputs.[..middleIndex-1] offset
+            | Higher -> loop inputs.[middleIndex+1..] (offset + middleIndex + 1)
+    loop inputs 0
