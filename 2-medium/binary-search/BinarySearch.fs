e479ad44235911e1e29951a66f813194560f1703
Romain Deneau <rdeneau@yahoo.fr>
2021-04-13 16:29:12 +0200

    Binary search v2

--- 2-medium/binary-search/BinarySearch.fs
+++ 2-medium/binary-search/BinarySearch.fs
@@ -1,20 +1,20 @@
 ﻿module BinarySearch
 
-type ComparisonResult = Lower | Equal | Higher
+type private Location = NotFound | Found | Left | Right
 
-let compare a b =
-    if a = b then Equal
-    elif a < b then Lower
-    else Higher
+let find inputs value =
+    let locate index inputs =
+        if inputs |> Array.isEmpty  then NotFound
+        elif value = inputs.[index] then Found
+        elif value < inputs.[index] then Left
+        else Right
 
-let find (inputs: int []) (value: int) =
-    let rec loop (inputs: int []) offset =
-        match inputs.Length with
-        | 0 -> None
-        | _ ->
-            let middleIndex = inputs.Length / 2
-            match compare value inputs.[middleIndex] with
-            | Equal  -> Some (offset + middleIndex)
-            | Lower  -> loop inputs.[..middleIndex-1] offset
-            | Higher -> loop inputs.[middleIndex+1..] (offset + middleIndex + 1)
+    let rec loop inputs offset =
+        let middleIndex = (inputs |> Array.length) / 2
+        match inputs |> locate middleIndex with
+        | NotFound -> None
+        | Found    -> Some (offset + middleIndex)
+        | Left     -> loop inputs.[..middleIndex-1] offset
+        | Right    -> loop inputs.[middleIndex+1..] (offset + middleIndex + 1)
+
     loop inputs 0
