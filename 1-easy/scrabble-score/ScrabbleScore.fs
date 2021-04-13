34c1ea24a9beeac99a0d6390fd6158083a144f2a
Romain Deneau <rdeneau@yahoo.fr>
2021-04-13 15:47:49 +0200

    Scrabble score

---
+++ 1-easy/scrabble-score/ScrabbleScore.fs
@@ -0,0 +1,16 @@
+﻿module ScrabbleScore
+
+let letterScore = function
+    | 'A' | 'E' | 'I' | 'O' | 'U'
+    | 'L' | 'N' | 'R' | 'S' | 'T' -> 1
+    | 'D' | 'G'                   -> 2
+    | 'B' | 'C' | 'M' | 'P'       -> 3
+    | 'F' | 'H' | 'V' | 'W' | 'Y' -> 4
+    | 'K'                         -> 5
+    | 'J' | 'X'                   -> 8
+    | 'Q' | 'Z'                   -> 10
+    | _ -> 0
+
+let score (word: string) =
+    word.ToUpperInvariant()
+    |> Seq.sumBy letterScore
