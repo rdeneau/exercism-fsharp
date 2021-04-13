03abce550d39e80767a18ad03b9b976c3bc66637
Romain Deneau <rdeneau@yahoo.fr>
2021-04-13 19:45:21 +0200

    Perfect number (easy-medium)

---
+++ 1-easy/perfect-numbers/PerfectNumbers.fs
@@ -0,0 +1,18 @@
+﻿module PerfectNumbers
+
+type Classification = Perfect | Abundant | Deficient
+
+let private aliquotSumOf n =
+    seq { 1 .. n/2 }
+    |> Seq.filter (fun factor -> n % factor = 0)
+    |> Seq.sum
+
+let private classifyPositive n =
+    let sum = aliquotSumOf n
+    if   n = sum then Perfect
+    elif n > sum then Deficient
+    else              Abundant
+
+let classify n =
+    if n <= 0 then None
+    else classifyPositive n |> Some
