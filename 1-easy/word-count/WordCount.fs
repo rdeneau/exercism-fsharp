0045e11f0fddd16972c87566e54fa29db5073f29
Romain Deneau <rdeneau@yahoo.fr>
2021-04-14 14:20:05 +0200

    Word count (w/o Regex)

---
+++ 1-easy/word-count/WordCount.fs
@@ -0,0 +1,28 @@
+﻿module WordCount
+
+open System
+
+let private unquote (word: string) =
+    word.Trim('\'')
+
+let private wordsOf (phrase: string) =
+    ([""], phrase.ToLower())
+    ||> Seq.fold (fun words c ->
+        if Char.IsControl(c) ||
+           (Char.IsPunctuation(c) && c <> '\'') ||
+           Char.IsSeparator(c) ||
+           Char.IsSymbol(c)
+        then
+            // Current word complete -> Prepare next word (currently empty)
+            "" :: words
+        else
+            // Append char to current word
+            $"{List.head words}{c}" :: (List.tail words)
+        )
+    |> List.filter (String.IsNullOrEmpty >> not)
+    |> List.map unquote
+
+let countWords phrase =
+    wordsOf phrase
+    |> List.countBy id
+    |> Map.ofSeq
