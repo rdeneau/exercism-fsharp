d294a1aeb50385cdad5f6b43b15eab9721bd3a3c
Romain Deneau <rdeneau@yahoo.fr>
2021-04-14 19:03:01 +0200

    Sieve (aka Prime number) (easy/medium)

---
+++ 1-easy/sieve/Sieve.fs
@@ -0,0 +1,13 @@
+﻿module Sieve
+
+let primes limit =
+    let maxMultiplier =
+        limit |> float |> sqrt |> floor |> int
+    let candidates =
+        if limit < 2
+        then []
+        else 2 :: [3 .. 2 .. limit]
+    (candidates, candidates)
+    ||> List.fold (fun primes candidate ->
+        let multiples = [ for multiplier in 2 .. maxMultiplier do candidate * multiplier ]
+        primes |> List.except multiples )
