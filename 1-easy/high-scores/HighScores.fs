2ba78d7e474424170894aa3368a2d8b7ba1e3d65
Romain Deneau <rdeneau@yahoo.fr>
2021-07-05 09:38:48 +0200

    high-scores green

--- 1-easy/high-scores/HighScores.fs
+++ 1-easy/high-scores/HighScores.fs
@@ -1,9 +1,15 @@
 ﻿module HighScores
 
-let scores (values: int list): int list = failwith "You need to implement this function."
+let scores (values: int list): int list =
+    values
 
-let latest (values: int list): int = failwith "You need to implement this function."
+let latest (values: int list): int =
+    values |> List.last
 
-let personalBest (values: int list): int = failwith "You need to implement this function."
+let personalBest (values: int list): int =
+    values |> List.max
 
-let personalTopThree (values: int list): int list = failwith "You need to implement this function."
+let personalTopThree (values: int list): int list =
+    values
+    |> List.sortDescending
+    |> List.truncate 3
