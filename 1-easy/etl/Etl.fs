7e2213e0ea3f97ed28ebba64bc52462ea6d65c96
Romain Deneau <rdeneau@yahoo.fr>
2021-07-05 10:55:24 +0200

    etl green

--- 1-easy/etl/Etl.fs
+++ 1-easy/etl/Etl.fs
@@ -1,3 +1,11 @@
 ﻿module Etl
 
-let transform (scoresWithLetters: Map<int, char list>): Map<char, int> = failwith "You need to implement this function."
+let private transformOne (score, letters) =
+    letters |> List.map (fun letter -> (letter |> System.Char.ToLower, score))
+
+let transform (scoresWithLetters: Map<int, char list>): Map<char, int> =
+    scoresWithLetters
+    |> Map.toSeq
+    |> Seq.collect transformOne
+    |> Seq.sortBy fst
+    |> Map.ofSeq
