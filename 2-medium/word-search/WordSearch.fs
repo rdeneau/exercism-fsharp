013979f1fa7008c2c0116005e36dddbfdacc4a17
Romain Deneau <rdeneau@yahoo.fr>
2021-07-15 21:36:18 +0200

    WordSearch: left to right

--- 2-medium/word-search/WordSearch.fs
+++ 2-medium/word-search/WordSearch.fs
@@ -1,8 +1,19 @@
 ﻿module WordSearch
 
 type Coordinate = int * int
+type Grid = string list
+
+let searchWord (grid: Grid) (word: string) : (Coordinate * Coordinate) option =
+    grid
+    |> List.indexed
+    |> List.tryPick
+        ( fun (y, line) ->
+             let x = line.IndexOf word
+             if x >= 0
+             then Some ((x+1, y+1), (x+word.Length, y+1))
+             else None )
 
 let search grid wordsToSearchFor : Map<string, (Coordinate * Coordinate) option> =
     wordsToSearchFor
-    |> List.map (fun x -> (x, None))
+    |> List.map (fun word -> (word, searchWord grid word))
     |> Map.ofList
