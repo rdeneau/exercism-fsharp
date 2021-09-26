7d7bf8c8561826524c0f5e2dd065957d7c6b5653
Romain Deneau <rdeneau@yahoo.fr>
2021-09-26 19:31:57 +0200

    optimize task3

--- 2-medium/codility-de/Task3.fs
+++ 2-medium/codility-de/Task3.fs
@@ -4,13 +4,6 @@ open System
 open Swensen.Unquote
 open Xunit
 
-let combine (items: string list) =
-    let rec loop acc rest =
-        match rest with
-        | [] -> acc
-        | x :: xs -> loop ((x :: acc) @ [for y in acc -> y + x]) xs
-    loop [] items
-
 let hasDistinctChars (s: String) =
     if String.IsNullOrEmpty s then
         false
@@ -18,31 +11,28 @@ let hasDistinctChars (s: String) =
         let s' = s |> Seq.distinct |> Seq.toArray |> String
         s = s'
 
+let combineDistinct (items: string list) =
+    let rec loop combinations rest =
+        match rest with
+        | [] -> combinations
+        | x :: rest' ->
+            if x |> hasDistinctChars then
+                let acc' =
+                    [ for y in combinations do
+                        yield y
+                        let yx = y + x
+                        if yx |> hasDistinctChars then
+                            yield yx ]
+                loop (x :: acc') rest'
+            else
+                loop combinations rest'
+    loop [] items
+
 let tryFindLongestCombination (items: string list) =
-    let combinations =
-        items
-        |> List.filter hasDistinctChars
-        |> combine
-        |> List.filter hasDistinctChars
+    let combinations = combineDistinct items
     if combinations.IsEmpty
     then None
     else Some (combinations |> List.maxBy String.length)
-
-[<Fact>]
-let ``Combine 0 item`` () =
-    combine [] =! []
-
-[<Fact>]
-let ``Combine 1 item`` () =
-    combine ["a"] =! ["a"]
-
-[<Fact>]
-let ``Combine 2 items`` () =
-    combine ["a"; "b"] =! ["b"; "a"; "ab"]
-
-[<Fact>]
-let ``Combine 3 items`` () =
-    combine ["a"; "b"; "c"] =! ["c"; "b"; "a"; "ab"; "bc"; "ac"; "abc"]
 
 [<Fact>]
 let ``"abc" has distinct chars`` () =
@@ -51,6 +41,10 @@ let ``"abc" has distinct chars`` () =
 [<Fact>]
 let ``"aaa" has not distinct chars`` () =
     hasDistinctChars "aaa" =! false
+
+[<Fact>]
+let ``Find no combination`` () =
+    tryFindLongestCombination ["aa"; "bbb"] =! None
 
 [<Fact>]
 let ``Find longest combination with distinct chars with [a, b, c]`` () =
